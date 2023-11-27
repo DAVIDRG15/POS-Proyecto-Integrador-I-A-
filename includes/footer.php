ea76fee6cc6960417ea33d51c03589b031d2763c
David Resendiz <132948326+DAVIDRG15@users.noreply.github.com>
2023-11-27 10:26:06 -0600

    update index and footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,16 @@ footer {
     height: 100px;
     background-color: #688391;
 }
+
+p {
+    color: white;
+}
 </style>
 
-<footer></footer>
+<footer>
+    <div class="container">
+        <br>
+        <p>Equipo 08</p>
+        <p>Grupo 004</p>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<!-- 
+    /*
+    Autor: Juan David Resendiz García
+    Fecha de Creación: 27 de noviembre de 2023
+    Descripción: Sistema Punto de Venta, proyecto para la materia Proyecto Integrador I A - 004
+    */
+ -->
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +16,11 @@
     <!-- CSS -->
     <link rel="stylesheet" href="assets/css/style.css">
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </head>
 
 <?php
@@ -23,8 +35,10 @@ include('includes/header.php');
         <br><br>
         <a href="views/sales/sales_form.php"><button type="button" class="btn btn-success btn1">Vender</button></a>
         <a href="views/sales/sales_list.php"><button type="button" class="btn btn-success btn1">Ventas</button></a>
-        <a href="views/product/product_list.php"><button type="button" class="btn btn-secondary btn2">Lista de Productos</button></a>
-        <a href="views/product/product_form.php"><button type="button" class="btn btn-secondary btn2">Administrar Productos</button></a>
+        <a href="views/product/product_list.php"><button type="button" class="btn btn-secondary btn2">Lista de
+                Productos</button></a>
+        <a href="views/product/product_form.php"><button type="button" class="btn btn-secondary btn2">Administrar
+                Productos</button></a>
     </div>
 </body>
 
